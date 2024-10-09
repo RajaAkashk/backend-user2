363083e6bb3dc84e71340711acc5a98132a110e6
Raja Akash Kumar <rajaakashk9@gmail.com>
2024-10-09 15:25:59 +0530

    update: Made changes to index.js

--- index.js
+++ index.js
@@ -10,35 +10,15 @@ intializeDatabase();
 const cors = require("cors");
 app.use(cors());
 
-// const fs = require("fs"); "to create a new movie."
-// const { release } = require("os");
-
-// const newMovie = {
-//   title: "RAJA AKASH",
-//   releaseYear: 2003,
-//   genre: ["Action"],
-//   director: "Aditya Roy Chopra",
-//   actors: ["Actor 1", "Actor 2"],
-//   language: "Hindi",
-//   country: "India",
-//   rating: 7.0,
-//   plot: "A young man and woman fall in love on a Indian trip.",
-//   awards: "IFA Filmfare Awards",
-//   posterUrl: "https://example.com/new-poster1.jpg",
-//   trailerUrl: "https://example.com/new-trailer1.mp4",
-// };
-
 async function createMovie(newMovie) {
   try {
     const hindiMovie = new Movie(newMovie);
     const saveMovie = await hindiMovie.save();
-    // console.log("New movie data: ", saveMovie);
     return saveMovie;
   } catch (error) {
     throw error;
   }
 }
-// createMovie(newMovie);
 app.post("/movies", async (req, res) => {
   try {
     const savedMovie = await createMovie(req.body);
@@ -50,13 +30,9 @@ app.post("/movies", async (req, res) => {
   }
 });
 
-// find a movie with particular title ---
-// (edit: getting movie by title using express)
 async function readMovieByTitle(movieTitle) {
   try {
     const movie = await Movie.findOne({ title: movieTitle });
-    // console.log("Movie data: ", movie); db learn ke liye tha
-    // se express se direct connect ke liye
     return movie;
   } catch (error) {
     throw error;
@@ -76,13 +52,10 @@ app.get("/movies/:title", async (req, res) => {
   }
 });
 
-// readMovieByTitle("Bajrangi Bhaijaan"); mongo
-
 // to get all the movies from database
 async function readAllMovies() {
   try {
     const allMovies = await Movie.find();
-    // console.log("All movies form database:", allMovies);
     return allMovies;
   } catch (error) {
     throw error;
@@ -105,14 +78,11 @@ app.get("/movies", async (req, res) => {
 async function readMovieByDirector(directorName) {
   try {
     const movieByDirector = await Movie.find({ director: directorName });
-    // const movieByDirector = await Movie.findOne({ director: directorName });
-    // console.log(movieByDirector);
     return movieByDirector;
   } catch (error) {
     console.log("Error while getting movie by director.", error);
   }
 }
-// readMovieByDirector("Kabir Khan");
 
 app.get("/movies/director/:directorName", async (req, res) => {
   try {
@@ -156,7 +126,6 @@ async function updateMovie(movieId, dataToUpdate) {
     const updatedMovie = await Movie.findByIdAndUpdate(movieId, dataToUpdate, {
       new: true,
     });
-    // console.log("Movie rating is updated", updatedMovie);
     return updatedMovie;
   } catch (error) {
     console.log("Error in updating the movie rating.", error);
@@ -179,8 +148,6 @@ app.post("/movies/:moviesId", async (req, res) => {
   }
 });
 
-// updateMovie("66ed0693758690323fbf4382", { plot: 'A young man and woman fall in love on a Punjab trip.' });
-
 // find one data and update its value
 async function updateMovieDetail(movieTitle, updatedRating) {
   try {
@@ -194,7 +161,6 @@ async function updateMovieDetail(movieTitle, updatedRating) {
     console.log("Error in changing data.", error);
   }
 }
-// updateMovieDetail("Dilwale Dulhania Le Jayenge", { rating: 9.0 });
 
 // find a movie by id and delete from database
 async function deleteMovie(movieId) {
@@ -215,7 +181,6 @@ app.delete("/movies/:movieId", async (req, res) => {
   }
 });
 
-// deleteMovie("66ed0693758690323fbf4388");
 async function deleteMovieByName(movieName) {
   try {
     const deleteMovie = await Movie.findOneAndDelete({ title: movieName });
@@ -224,37 +189,6 @@ async function deleteMovieByName(movieName) {
     console.log("Error in deleting movie.", error);
   }
 }
-// deleteMovieByName("New MOvie");
-
-// const jsonData = fs.readFileSync("movies.json", "utf-8");
-// const moviesData = JSON.parse(jsonData);
-
-//this is done to send data from movies.json to mongo db server.
-
-// function seedData() {
-//   try {
-//     for (const movieData of moviesData) {
-//       const newMovie = new Movie({
-//         title: movieData.title,
-//         releaseYear: movieData.releaseYear,
-//         genre: movieData.genre,
-//         director: movieData.director,
-//         actors: movieData.actors,
-//         language: movieData.language,
-//         country: movieData.country,
-//         rating: movieData.rating,
-//         plot: movieData.plot,
-//         awards: movieData.awards,
-//         posterUrl: movieData.posterUrl,
-//         trailerUrl: movieData.trailerUrl,
-//       });
-//       newMovie.save();
-//     }
-//   } catch (error) {
-//     console.log("Error seeding the data", error);
-//   }
-// }
-// seedData();
 
 const PORT = 3000;
 
